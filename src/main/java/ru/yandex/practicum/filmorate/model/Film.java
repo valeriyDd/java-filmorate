559f70fee77b9b9070b36696459dcd9f937ed514
VALL <vallall122198@gmail.com>
2023-04-11 17:47:50 +0300

    добавил жанр и рейтинг

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,24 +1,40 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.validator.FilmReleaseDateConstraint;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Data
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class Film {
-    private int id;
-    @NotBlank
-    private final String name;
-    @Size(max = 200)
-    private final String description;
-    private final LocalDate releaseDate;
-    @Positive
-    private final long duration;
-    private Set<Integer> likes;
+    Long id;
+
+    @NotNull @NotBlank String name;
+
+    @Size(max = 200) String description;
+
+    @NotNull
+    @FilmReleaseDateConstraint
+    LocalDate releaseDate;
+
+    @Positive Integer duration;
+
+    @JsonIgnore
+    Set<Long> likes = new HashSet<>();
+    Mpa mpa;
+    Set<Genre> genres = new LinkedHashSet<>();
 }
